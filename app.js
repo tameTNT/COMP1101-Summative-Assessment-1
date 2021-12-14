5cba2326042df7887c437742c0484ffd416c52d6
Luca Huelle <lucahuelle@gmail.com>
2021-12-14 00:01:54 +0000

    📦 Add luxon datetime package
    Modern alternative to moment.js for handling datetime objects.
    Can handle timezone conversion and formatting etc.

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
+// const { DateTime } = require('luxon');
+
 const express = require('express');
 const app = express();
 
 app.use(express.static('client'));
+
+// console.log(JSON.stringify(DateTime.now().toUTC()));
+// console.log(DateTime.fromISO(JSON.parse(JSON.stringify('2021-12-13T22:16:28.278Z'))));
 
 module.exports = app;
