4239b1b7f91dad09bb55d393a92b1448cec28abc
Luca Huelle <lucahuelle@gmail.com>
2021-12-18 14:33:27 +0000

    ✨ Add modal to submit new post
    Floating new post button now brings up form to add new post.
    highlight.js functionality is now done in index.js and not within html
    script tags.

--- client/index.js
+++ client/index.js
@@ -1,1 +1,21 @@
-console.log('Deferred script loaded');
+console.log('Deferred script loading');
+
+// const textFields = Array.from(document.getElementsByClassName('code-text'));
+
+document.querySelectorAll('pre code').forEach((el) => {
+  hljs.highlightElement(el);
+});
+
+const codeInputArea = document.getElementById('postCodeInput');
+const codePreviewArea = document.getElementById('codeInputPreview');
+
+function highlightCodeInput (newText) {
+  codePreviewArea.innerHTML = newText;
+  hljs.highlightElement(codePreviewArea);
+}
+
+highlightCodeInput(codeInputArea.innerText); // initial highlighting on DOM load
+
+codeInputArea.addEventListener('input', (e) => {
+  highlightCodeInput(e.target.value);
+});
