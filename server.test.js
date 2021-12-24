794334920eaf5cb9f639f6a23a13ff26ba590fae
Luca Huelle <lucahuelle@gmail.com>
2021-12-24 13:21:08 +0000

    ✅ Change structure of JEST tests
    Tests are now completed using .forEach() to make code DRYer and expect()
    functions are used to test various parts of response.

--- server.test.js
+++ server.test.js
@@ -1,10 +1,23 @@
 const request = require('supertest');
 const app = require('./app');
 
-describe('Test basic GET methods', () => {
+const testEntity = async function (entity) {
+  const response = await request(app).get(`/${entity}/`);
+  expect(response.statusCode).toBe(200);
+  expect(response.headers['content-type']).toEqual(expect.stringContaining('json'));
+  expect(JSON.parse(response.text)).toEqual(require('./serverdb.json')[entity]);
+};
+
+describe('Test GET methods', () => {
   test('GET / succeeds', async () => {
-    return request(app)
-        .get('/')
-        .expect(200);
+    const response = await request(app).get('/');
+    expect(response.statusCode).toBe(200);
+    expect(response.headers['content-type']).toEqual(expect.stringContaining('html'));
   });
+
+  ['comments', 'cards'].forEach(
+    (entity) => {
+      test(`GET /${entity}/ returns all ${entity}`, () => testEntity(entity));
+    }
+  );
 });
