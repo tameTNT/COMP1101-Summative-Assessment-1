efb1923edde68cef0999c694f51536fdfb6405bb
Luca Huelle <lucahuelle@gmail.com>
2021-12-26 20:13:57 +0000

    ✨ Allow users to complete form and create new posts
    A user can now create a new card by filling out the modal form which
    then POSTs via JSON to the API to create a new card object in the
    database.

--- client/index.js
+++ client/index.js
@@ -16,3 +16,55 @@ codeInputArea.addEventListener('input', (e) => {
   codePreviewArea.innerHTML = e.target.value;
   hljs.highlightElement(codePreviewArea);
 });
+
+// initial highlighting on DOM load
+highlightAllCode();
+codePreviewArea.innerHTML = "print('Hello World')";
+hljs.highlightElement(codePreviewArea);
+
+// todo: change highlighting class on form selection change
+
+async function postFormDataToUrlAsJSON (url, formData) {
+  const plainData = Object.fromEntries(formData.entries());
+  const formDataJSONString = JSON.stringify(plainData);
+
+  const fetchOptions = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json'
+    },
+    body: formDataJSONString
+  };
+
+  await fetch(url, fetchOptions);
+}
+
+async function formSubmitListener (event) {
+  event.preventDefault();
+
+  const form = event.currentTarget;
+  const formData = new FormData(form);
+
+  let apiPOSTUrl = '';
+  if (form.id === 'newCardForm') {
+    apiPOSTUrl = 'http://127.0.0.1:8000/cards';
+  } else if (form.id === 'newCommentForm') {
+    apiPOSTUrl = 'http://127.0.0.1:8000/comments';
+  }
+
+  try {
+    await postFormDataToUrlAsJSON(apiPOSTUrl, formData);
+  } catch (error) {
+    console.error(error);
+  }
+
+  // Who knows why this doesn't work - pulled my hair out tyring to get it do so...
+  // const newCardModal = new bootstrap.Modal(document.getElementById('newCardModal'));
+  // newCardModal.hide();
+  document.getElementById('newCardFormModalCloseButton').click();
+  form.reset(); // clear form values
+  // todo: do reloading page elements stuff for dynamic page
+}
+
+document.getElementById('newCardForm').addEventListener('submit', formSubmitListener);
