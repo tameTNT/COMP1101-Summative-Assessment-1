c31535ad1fce81006db5a0904e6fe5e1df056330
Luca Huelle <lucahuelle@gmail.com>
2021-12-26 20:12:37 +0000

    🎨 Reorganise highlighting functions

--- client/index.js
+++ client/index.js
@@ -2,20 +2,17 @@ console.log('Deferred script loading');
 
 // const textFields = Array.from(document.getElementsByClassName('code-text'));
 
-document.querySelectorAll('pre code').forEach((el) => {
-  hljs.highlightElement(el);
-});
-
-const codeInputArea = document.getElementById('postCodeInput');
-const codePreviewArea = document.getElementById('codeInputPreview');
-
-function highlightCodeInput (newText) {
-  codePreviewArea.innerHTML = newText;
-  hljs.highlightElement(codePreviewArea);
+// standard code highlighting across page
+function highlightAllCode () {
+  document.querySelectorAll('pre code').forEach((el) => {
+    hljs.highlightElement(el);
+  });
 }
 
-highlightCodeInput(codeInputArea.innerText); // initial highlighting on DOM load
-
+// highlighting live code preview field
+const codeInputArea = document.getElementById('newCardCodeInput');
+const codePreviewArea = document.getElementById('codeInputPreview');
 codeInputArea.addEventListener('input', (e) => {
-  highlightCodeInput(e.target.value);
+  codePreviewArea.innerHTML = e.target.value;
+  hljs.highlightElement(codePreviewArea);
 });
