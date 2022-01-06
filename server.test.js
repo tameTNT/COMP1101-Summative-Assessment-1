eb098ba0b985bcefe4a4fd66b0b2faada04fc972
Luca Huelle <lucahuelle@gmail.com>
2022-01-06 17:25:03 +0000

    🐛 Fix entity tests in server.test.js
    The database json was being read before the request had finished meaning
     that reddit data was highly likely to differ between the response and
    the database leading the .toEqual() test to fail.
    Now the database is only loaded once the response has been awaited.

--- server.test.js
+++ server.test.js
@@ -1,11 +1,21 @@
+const fs = require('fs');
+
 const request = require('supertest');
 const app = require('./app');
 
-const testEntity = async function (entity) {
-  const response = await request(app).get(`/${entity}/`);
-  expect(response.statusCode).toBe(200);
-  expect(response.headers['content-type']).toEqual(expect.stringContaining('json'));
-  expect(JSON.parse(response.text)).toEqual(require('./serverdb.json')[entity]);
+const testEntity = function (entity) {
+  request(app).get(`/${entity}/`).then((response) => {
+    expect(response.statusCode).toBe(200);
+    expect(response.headers['content-type']).toEqual(expect.stringContaining('json'));
+    fs.readFile('./serverdb.json', 'utf8', async (err, fileData) => {
+      if (err) {
+        console.error(err);
+      } else {
+        const entityData = JSON.parse(fileData)[entity];
+        expect(JSON.parse(response.text)).toEqual(entityData);
+      }
+    });
+  });
 };
 
 describe('Test GET methods', () => {
@@ -15,9 +25,6 @@ describe('Test GET methods', () => {
     expect(response.headers['content-type']).toEqual(expect.stringContaining('html'));
   });
 
-  ['comments', 'cards'].forEach(
-    (entity) => {
-      test(`GET /${entity}/ returns all ${entity}`, () => testEntity(entity));
-    }
-  );
+  test('GET /cards/ returns all cards', () => testEntity('cards'));
+  test('GET /comments/ returns all comments', () => testEntity('comments'));
 });
