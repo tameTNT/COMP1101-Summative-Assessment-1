46c60a7fa2e7dbd81d013b03f5a5badea5248eb8
Luca Huelle <lucahuelle@gmail.com>
2022-01-09 18:31:49 +0000

    🎨 Improve readability of server.test.js
    Add a separate function to get .json data from disk.
    Add 'status' to test descriptions when status codes are returned.

--- server.test.js
+++ server.test.js
@@ -12,9 +12,13 @@ expect.extend({ toMatchCloseTo });
 // }
 const matchPrecision = -Math.log10(20); // will result in a match tolerance of ±10
 
+const getDbData = function () {
+  return JSON.parse(fs.readFileSync('./serverdb.json', 'utf8'));
+};
+
 let realDatabaseData = {};
 beforeAll(() => {
-  realDatabaseData = JSON.parse(fs.readFileSync('./serverdb.json', 'utf8'));
+  realDatabaseData = getDbData();
   const testDatabaseStr = fs.readFileSync('./serverdb.test.json', 'utf8');
   fs.writeFileSync('./serverdb.json', testDatabaseStr, 'utf-8');
 });
@@ -24,7 +28,7 @@ const testEntityBasic = async function (entityName) {
   expect(response.status).toEqual(200);
   expect(response.headers['content-type']).toMatch(/json/);
 
-  const fileData = JSON.parse(fs.readFileSync('./serverdb.json', 'utf8'));
+  const fileData = getDbData();
   expect(JSON.parse(response.text)).toEqual(fileData[entityName]);
 };
 
@@ -43,10 +47,10 @@ const testEndpointArrayLength = async function (endpoint, expLength) {
 };
 
 const testEndpointsSameResponse = async function (endpointA, endpointB) {
-  let response = await request(app).get(`/${endpointA}`);
-  const byQuery = JSON.parse(response.text);
-  response = await request(app).get(`/${endpointB}`);
-  const byParam = JSON.parse(response.text);
+  const responseA = await request(app).get(`/${endpointA}`);
+  const byQuery = JSON.parse(responseA.text);
+  const responseB = await request(app).get(`/${endpointB}`);
+  const byParam = JSON.parse(responseB.text);
   // These requests invoke getRedditData() on the server side so Reddit JSON data is fetched
   // The data Reddit returns can actually change between the two requests
   // (specifically 'score' - because of vote fuzzing: https://www.reddit.com/r/redditdev/comments/kgwai/reddit_api_confused_about_score/)
@@ -68,7 +72,7 @@ describe('Test GET methods', () => {
     test('GET /cards?ids=1,10 returns array of one card', () => testEndpointArrayLength('cards?ids=1,10', 1));
     test('GET /cards?ids=1,2 returns array of two cards', () => testEndpointArrayLength('cards?ids=1,2', 2));
     test('GET /cards?ids=1 returns same card object as /cards/1', () => testEndpointsSameResponse('cards?ids=1', 'cards/1'));
-    test('GET /cards/10 returns 404', () => testEndpoint404('cards/10', 'card'));
+    test('GET /cards/10 returns status 404', () => testEndpoint404('cards/10', 'card'));
 
     describe('Test /reddit extending endpoint', () => {
       test('GET /cards/1/reddit returns Reddit json data', async () => {
@@ -76,7 +80,7 @@ describe('Test GET methods', () => {
         expect(response.status).toEqual(200);
         expect(response.headers['content-type']).toMatch(/json/);
       });
-      test('GET /cards/10/reddit returns 404', () => testEndpoint404('cards/10/reddit', 'card'));
+      test('GET /cards/10/reddit returns status 404', () => testEndpoint404('cards/10/reddit', 'card'));
     });
   });
 
@@ -87,7 +91,7 @@ describe('Test GET methods', () => {
     test('GET /comments?ids=1,10 returns array of one comment', () => testEndpointArrayLength('comments?ids=1,10', 1));
     test('GET /comments?ids=1,2 returns array of two comments', () => testEndpointArrayLength('comments?ids=1,2', 2));
     test('GET /comments?ids=1 returns same card object as /comments/1', () => testEndpointsSameResponse('comments?ids=1', 'comments/1'));
-    test('GET /comments/10 returns 404', () => testEndpoint404('comments/10', 'comment'));
+    test('GET /comments/10 returns status 404', () => testEndpoint404('comments/10', 'comment'));
   });
 });
 
